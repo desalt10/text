849851f79f8da22db88791cbe7c4ffea966360af
desalt10 <648377611@qq.com>
2020-07-31 17:42:20 +0800

    index.php

---
+++ H5-test/index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title></title>
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<link rel="stylesheet" href="css/sassConfig.css" />
+		<link rel="stylesheet" href="css/common.css" />
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" href="font/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="font/iconfont/iconfont.css">
+		<link rel="stylesheet" href="css/main.css" />
+		
+		<div id="sheet1" class="mui-popover mui-popover-bottom mui-popover-action">
+			<!-- 可选择菜单 -->
+			<ul class="mui-table-view">
+			  <li class="mui-table-view-cell">
+				<a href="#"></a>
+			  </li>
+			  <li class="mui-table-view-cell">
+				<!-- <a href="#">2菜单2</a> -->
+			  </li>
+			</ul>
+			<!-- 取消菜单 -->
+			<ul class="mui-table-view">
+			  <li class="mui-table-view-cell">
+				<!-- <a href="#sheet1"><b>3取消</b></a> -->
+			  </li>
+			</ul>
+		</div>
+	</head>
+	<body>
+		<div class="container">
+			<header class="clearfix">
+				<h1 class="logo pull-left" >
+					<a href="index.html" title="朗朗口腔" target="_blank">
+						<img src="img/logo.jpg"  alt="logo">
+					</a>
+				</h1>
+				<div class="pull-right top-menu">
+					<div class="pull-right">
+						<div class="menu-link clearfix">
+							<div class="lang">中文
+								<ul>
+									<a href="">中文</a>
+									<a href="">英文</a>
+								</ul>
+							</div>
+							<span class="link"></span>
+							<a href="tel:0755-86566573" title="0755-86566573">0755-86566573</a>
+							<span class="link"></span>
+							<a href="index.html" title="朗朗口腔集团官网">朗朗口腔集团官网</a>
+						</div>
+						<nav>
+							<ul class="clearfix">
+								<li class="current">
+									<a href="index.html" title="首页">首页</a></li>
+								<li>
+									<a href="">走进朗朗</a>
+									<ul>
+										<li><a href="">朗朗简介</a></li>
+										<li><a href="">朗朗优势</a></li>
+										<li><a href="">核心团队</a></li>
+										<li><a href="">朗朗历程</a></li>
+									</ul>
+								</li>
+								<li><a>咨询动态</a></li>
+								<li><a>合作伙伴</a></li>
+								<li><a>辅助功能</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+		</div>
+		<!-- <img src="img/img.jpg" width="100%" alt=""> -->
+		<!-- 幻灯片 start -->
+		<div id="slide-wrap">
+			<div class="slide-content">
+				<ul class="clearfix">
+					<li><img src="img/img.jpg" alt="" width="100%"></li>
+					<li><img src="img/img.jpg" alt="" width="100%"></li>
+					<li><img src="img/img.jpg" alt="" width="100%"></li>
+				</ul>
+				
+			</div>
+			<ul class="slide-nav">
+				<a href="javascript: void(0)" class="current"></a>
+				<a href="javascript: void(1)"></a>
+				<a href="javascript: void(2)"></a>
+			</ul>
+			<span class="slide-prev">
+				<i class="iconfont icon-jiantou3"></i>
+			</span>
+			<span class="slide-next">
+				<i class="iconfont icon-jiantou"></i>
+			</span>
+		</div>
+		<!-- 幻灯片 end -->
+		<div class="container">
+			<div class="main-column">
+				<h3>朗朗口腔</h3>
+				<p>
+					呵护牙齿健康  创造美好未来
+				<span></span>
+					care for teeth to create a better future
+				</p>
+			</div>
+			
+			<div class="info-list">
+				<div id="infowListWrap">
+					<!-- <div class="loading-wait">
+					加载中
+					<div class="loading-icon"><i class="iconfont icon-jiazai"></i></div> -->
+					</div>
+				
+				</div>
+				<!-- <ul class="clearfix">
+					<figure>
+						<img src="img/pic001.jpg" alt="朗朗口腔">
+						<figcaption>朗朗口腔</figcaption>
+						<p>诊所内</p>
+					</figure>
+					<figure>
+						<img src="img/pic001.jpg" alt="舒适的就诊环境">
+						<figcaption>舒适的就诊环境</figcaption>
+						<p>诊所内</p>
+					</figure>
+					<figure>
+						<img src="img/pic001.jpg" alt="高端的诊疗设备">
+						<figcaption>高端的诊疗设备</figcaption>
+						<p>朗朗口腔</p>
+					</figure>
+				</ul> -->
+				<a href="" class="more-link">查看更多</a>
+			</div>
+			
+		</div>
+		<footer>
+			<div class="container">
+				<div class="clearfix">
+					<div class="pull-left">
+						<address class="footer-contact-info">
+							<h5>朗朗医疗投资有限公司</h5>
+							<p>集团总部热线 :<a title="" href="tel:4000-890-608"></a> </p>
+							<p>集团总部地址：广东省深圳市南山区高新南一道3号富诚大厦三楼</p>
+						</address>
+						<ul class="share clearfix">
+							<a href="" class="sing-share">
+								<p>
+									<img src="img/code.jpg" alt="">
+								</p>
+								<i class="fa fa-weibo"></i>
+							</a>
+							<a href="" class="wechat-share">
+								<p>
+									<img src="img/code.jpg" alt="">
+								</p>
+								<i class="fa fa-weixin"></i>
+							</a>
+						</ul>
+					</div>
+					<div class="pull-right">
+						<ul class="footer-menu clearfix">
+							<dl>
+								<dt>走进朗朗</dt>
+								<dd><a title="朗朗简介" href="">朗朗简介</a></dd>
+								<dd><a title="朗朗优势" href="">朗朗优势</a></dd>
+								<dd><a title="企业荣誉" href="">企业荣誉</a></dd>
+								<dd><a title="团队风采" href="">团队风采</a></dd>
+							</dl>
+							<dl>
+								<dt>咨询动态</dt>
+								<dd><a title="行业动态" href="">行业动态</a></dd>
+						      	<dd><a title="朗朗资讯" href="">朗朗资讯</a></dd>
+								<dd><a title="媒体报道" href="">媒体报道</a></dd>
+								<dd><a title="公益活动" href="">公益活动</a></dd>
+							</dl>
+							<dl>
+								<dt>合作方案</dt>
+								<dd><a title="门诊列表" href="">门诊列表</a></dd>
+								<dd><a title="门诊详情" href="">门诊详情</a></dd>
+								<dd><a title="交通指引" href="">交通指引</a></dd>
+							</dl>
+							<dl>
+								<dt>辅助功能</dt>
+								<dd><a title="公众号指引" href="">公众号指引</a></dd>
+								<dd><a title="交通指南" href="">交通指南</a></dd>
+								<dd><a title="在线客服" href="">在线客服</a></dd>
+								<dd><a title="一键拨号" href="">一键拨号</a></dd>
+							</dl>  
+							<dl>
+								<div class="wechat-code">
+									<img src="img/code.jpg" alt="">
+									<p class="text-center">微信公众号</p>
+								</div>
+							</dl>
+						</ul>
+					</div>
+				</div>
+				<div id="copyright">© 2005 - 2016 朗朗口腔医疗投资有限公司 版权所有 粤ICP备08130115号-1</div>
+			</div>
+		</footer>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/common.js"></script>
+		<script src="js/banner.js"></script>
+		<script>
+			loadingMsg({
+				id: 'infowListWrap',
+				message:'加载中'
+			})
+			$.ajax({
+			    url: "./data/index.php",    //请求的url地址
+			    dataType: "json",   //返回格式为json
+			    async: true, //请求是否异步，默认为异步，这也是ajax重要特性
+			    cache: false, // 是否读取缓存
+			    data: { 
+					categoryId: 123456,
+				},    //参数值
+			    type: "POST",   //请求方式
+			    beforeSend: function() {
+			    //请求前的处理
+			    },
+			    success: function(req) { //请求成功时处理
+				//判断数据是否存在
+				let infowListWrap = document.getElementById('infowListWrap');
+				if(!req.data|| req.data.length === 0){
+					// infowListWrap.innerHTML =`<div class="loading-wait">当前没有数据</div>`
+					loadingMsg({
+						id: 'infowListWrap',
+						message:'当前没有数据'
+					})
+					return false
+				}
+				
+				let ul = '<ul class="clearfix">'
+					req.data.forEach(item=>{
+						ul += `<figure>
+							<a href="newsDetail.php?id=${item.id}">
+							<img src="${item.imgUrl}" alt="${item.title}">
+							<figcaption>${item.title}</figcaption>
+							<p>${item.dec}</p>
+							</a>
+							</figure>`
+					})
+					ul += '</ul>'
+					infowListWrap.innerHTML = ul
+			    },
+			    complete: function() {
+			    //请求完成的处理
+			    },
+			    error: function() {
+			    //请求出错处理
+			    }
+			});
+		</script>
+	</body>
+</html> 
